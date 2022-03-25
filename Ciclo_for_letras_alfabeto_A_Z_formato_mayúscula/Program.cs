a9a04c616c99657c19cebde52f0c3254ff1b8886
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:25:49 -0600

    Agregar archivos de proyecto.

---
+++ Ciclo_for_letras_alfabeto_A_Z_formato_mayúscula/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//For simple
+char letra;
+for (letra = 'A'; letra <= 'Z'; letra++)
+{
+    Console.WriteLine(letra);
+}
+Console.ReadKey(true);
